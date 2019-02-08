0197de99b2557456c2fa24bd4bcb8d539f3f3a46
Turtleman1024 <kmurry6262@gmail.com>
2019-02-08 15:02:33 -0800

    Creating a desk of cards using an array of card objects.

--- script.js
+++ script.js
@@ -10,8 +10,7 @@ let values = ['Ace', 'King', 'Queen', 'Jack', 'Ten',
               'Nine', 'Eight', 'Seven', 'Six', 'Five',
               'Four', 'Three', 'Two'];
 
-let deck = [];
-
+//A method that will create a deck of cards
 function createDeck()
 {
     let deck = []
@@ -21,22 +20,36 @@ function createDeck()
         //Loop through each value
         for(let valuesIndex = 0; valuesIndex < values.length; valuesIndex++)
         {
-            //Assign the value to a suit and store it in the deck array.
-            deck.push(values[valuesIndex] + ' of ' + suits[suitIndex])
+            //Create a card object and store the suit and value in the card
+            let card = {
+                suit: suits[suitIndex],
+                value: values[valuesIndex]
+            };
+            //Added the card to the deck array (Array of objects)
+            deck.push(card);
         }
     }
     return deck;   
 }
 
+//A method that will return the card string
+function getCardString(card)
+{
+    return card.value + ' of ' + card.suit;
+}
+
+//A method that will return the next card on the deck
 function getNextCard()
 {
     return deck.shift();
 }
 
-deck = createDeck();
+//Create a deck of cards
+let deck = createDeck();
 
-for(let deckIndex = 0; deckIndex < deck.length; deckIndex++)
-{
-    console.log(deck[deckIndex]);
-}
+//Assign two cards to a player
+let playerCards = [getNextCard(), getNextCard()];
 
+//Display the two cards
+console.log(" " + getCardString(playerCards[0]));
+console.log(" "+ getCardString(playerCards[1]));
